d5e58c179455f8914f732f56ccba190e80b7568d
Hudo Assenco <hudo.assenco@gmail.com>
2016-08-01 22:45:48 -0700

    Remove webpack-dev-server

--- cloud/webpack.config.js
+++ cloud/webpack.config.js
@@ -2,15 +2,13 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
     './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'react-hot!babel'
+      loader: 'babel'
     }]
   },
   resolve: {
@@ -20,12 +18,5 @@ module.exports = {
     path: __dirname + '/public',
     publicPath: '/public',
     filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './public',
-    hot: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+  }
 };
